e3535a174e94b7928f32894209e1172f02e5dbe8
Bianca Niemann <100941469+BiancaNiemann@users.noreply.github.com>
2025-09-01 16:39:14 +0200

    Create Library_database.sql

---
+++ Library_database.sql
@@ -0,0 +1,157 @@
+use role sysadmin;
+
+// Create a new database and set the context to use the new database
+create database library_card_catalog comment = 'DWW Lesson 10 ';
+
+//Set the worksheet context to use the new database
+use database library_card_catalog;
+
+create or replace table book
+( book_uid number autoincrement
+ , title varchar(50)
+ , year_published number(4,0)
+);
+
+// Insert records into the book table
+// You don't have to list anything for the
+// BOOK_UID field because the AUTOINCREMENT property 
+// will take care of it for you
+
+insert into book(title, year_published)
+values
+ ('Food',2001)
+,('Food',2006)
+,('Food',2008)
+,('Food',2016)
+,('Food',2015);
+
+// Check your table. Does each row have a unique id? 
+select * from book;
+
+create or replace table author (
+   author_uid number 
+  ,first_name varchar(50)
+  ,middle_name varchar(50)
+  ,last_name varchar(50)
+);
+
+// Insert the first two authors into the Author table
+insert into author(author_uid, first_name, middle_name, last_name)  
+values
+(1, 'Fiona', '','Macdonald')
+,(2, 'Gian','Paulo','Faleschini');
+
+// Look at your table with it's new rows
+select * 
+from author;
+
+// Create the relationships table
+// this is sometimes called a "Many-to-Many table"
+create table book_to_author
+( book_uid number
+  ,author_uid number
+);
+
+//Insert rows of the known relationships
+insert into book_to_author(book_uid, author_uid)
+values
+ (1,1)  // This row links the 2001 book to Fiona Macdonald
+,(1,2)  // This row links the 2001 book to Gian Paulo Faleschini
+,(2,3)  // Links 2006 book to Laura K Egendorf
+,(3,4)  // Links 2008 book to Jan Grover
+,(4,5)  // Links 2016 book to Jennifer Clapp
+,(5,6); // Links 2015 book to Kathleen Petelinsek
+
+
+//Check your work by joining the 3 tables together
+//You should get 1 row for every author
+select * 
+from book_to_author ba 
+join author a 
+on ba.author_uid = a.author_uid 
+join book b 
+on b.book_uid=ba.book_uid; 
+
+
+// Create an Ingestion Table for JSON Data
+create table library_card_catalog.public.author_ingest_json
+(
+  raw_author variant
+);
+
+//Create File Format for JSON Data 
+create or replace file format library_card_catalog.public.json_file_format
+type = 'JSON' 
+compression = 'AUTO' 
+enable_octal = FALSE
+allow_duplicate = FALSE 
+strip_outer_array = TRUE
+strip_null_values = FALSE
+ignore_utf8_errors = FALSE; 
+
+select $1
+from @util_db.public.my_internal_stage/author_with_header.json
+(file_format => library_card_catalog.public.json_file_format );
+
+copy into LIBRARY_CARD_CATALOG.PUBLIC.AUTHOR_INGEST_JSON
+from @util_db.public.my_internal_stage
+files = ( 'author_with_header.json')
+file_format = ( format_name=library_card_catalog.public.json_file_format );
+
+//returns AUTHOR_UID value from top-level object's attribute
+select raw_author:AUTHOR_UID
+from author_ingest_json;
+
+//returns the data in a way that makes it look like a normalized table
+SELECT 
+ raw_author:AUTHOR_UID
+,raw_author:FIRST_NAME::STRING as FIRST_NAME
+,raw_author:MIDDLE_NAME::STRING as MIDDLE_NAME
+,raw_author:LAST_NAME::STRING as LAST_NAME
+FROM AUTHOR_INGEST_JSON;
+
+// Create an Ingestion Table for the NESTED JSON Data
+create or replace table library_card_catalog.public.nested_ingest_json 
+(
+  raw_nested_book VARIANT
+);
+
+
+select $1
+from @util_db.public.my_internal_stage/json_book_author_nested.txt
+(file_format => library_card_catalog.public.json_file_format );
+
+copy into LIBRARY_CARD_CATALOG.PUBLIC.NESTED_INGEST_JSON
+from @util_db.public.my_internal_stage
+files = ( 'json_book_author_nested.txt')
+file_format = ( format_name=library_card_catalog.public.json_file_format );
+
+//a few simple queries
+select raw_nested_book
+from nested_ingest_json;
+
+select raw_nested_book:year_published
+from nested_ingest_json;
+
+select raw_nested_book:authors
+from nested_ingest_json;
+
+//Use these example flatten commands to explore flattening the nested book and author data
+select value:first_name
+from nested_ingest_json
+,lateral flatten(input => raw_nested_book:authors);
+
+select value:first_name
+from nested_ingest_json
+,table(flatten(raw_nested_book:authors));
+
+//Add a CAST command to the fields returned
+SELECT value:first_name::varchar, value:last_name::varchar
+from nested_ingest_json
+,lateral flatten(input => raw_nested_book:authors);
+
+//Assign new column  names to the columns using "AS"
+select value:first_name::varchar as first_nm
+, value:last_name::varchar as last_nm
+from nested_ingest_json
+,lateral flatten(input => raw_nested_book:authors);
